03fbc418190397c592c4adade68b953e30f4e5bf
Francisco M.C.M. <fcasmen658@gmail.com>
2024-11-12 14:14:26 +0100

    Enum Curso terminado

---
+++ src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Curso.java
@@ -0,0 +1,21 @@
+package org.iesalandalus.programacion.matriculacion.dominio;
+
+public enum Curso {
+    PRIMERO("Primero"),
+    SEGUNDO("Segundo");
+
+    private final String cadenaAMostrar;
+
+    Curso(String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    public String imprimir() {
+        return cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return cadenaAMostrar;
+    }
+}
